b1408bbc7239d7f727158c1af14193ea1e5a1fb4
naveenmvoff <naveenmvoff@gmail.com>
2024-10-24 13:14:04 +0530

    JS Foundation - Simple Primitives in JS

---
+++ 0-100/Week1/js_foundation/main.js
@@ -0,0 +1,13 @@
+let firstname = "Naveen";
+let age = 20;
+let isMarried = false;
+
+// Get Input From user need more command 
+// let firstname = prompt("Enter the name: ");
+// let age = prompt("Enter your age: ");
+// let isMarried = prompt("is you are married (yes/no)");
+
+// age = Number(age);
+// isMarried = (isMarried.toLowerCase() === 'yes');
+
+console.log("This person's name is " + firstname + " and the age " + age + " then the person is " + (isMarried ? "Married":"Unmarried") );
