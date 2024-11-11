8f52bca035cb1674c1b5d909bd66c0d39ec62220
naveenmvoff <naveenmvoff@gmail.com>
2024-11-11 18:58:21 +0530

    Upto simple primitives

--- 0-100/Week1/js_foundation/main.js
+++ 0-100/Week1/js_foundation/main.js
@@ -1,13 +1,32 @@
-let firstname = "Naveen";
-let age = 20;
-let isMarried = false;
+// let myName = "Naveen";
+// let age = 18;
+// let isMarried = false;
 
-// Get Input From user need more command 
-// let firstname = prompt("Enter the name: ");
-// let age = prompt("Enter your age: ");
-// let isMarried = prompt("is you are married (yes/no)");
+// if (isMarried == true){
+//     console.log("The person " + myName + " age is " + age + " and the person is married")
+// } else{
+//     console.log("The person " + myName + " age is " + age + " and the person is not married")
+// }
 
-// age = Number(age);
-// isMarried = (isMarried.toLowerCase() === 'yes');
 
-console.log("This person's name is " + firstname + " and the age " + age + " then the person is " + (isMarried ? "Married":"Unmarried") );
+// // const a =  1;
+// // a = 2;
+// // console.log(a)
+
+
+
+
+
+// // let firstname = "Naveen";
+// // let age = 20;
+// // let isMarried = false;
+
+// // // Get Input From user need more command 
+// // // let firstname = prompt("Enter the name: ");
+// // // let age = prompt("Enter your age: ");
+// // // let isMarried = prompt("is you are married (yes/no)");
+
+// // // age = Number(age);
+// // // isMarried = (isMarried.toLowerCase() === 'yes');
+
+// // console.log("This person's name is " + firstname + " and the age " + age + " then the person is " + (isMarried ? "Married":"Unmarried") );
